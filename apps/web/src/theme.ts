7df694b9bced4ccc0b99b99a55e5acff3e9726bd
Sonny Nguyen <sonny.nguyen.150101@gmail.com>
2026-04-23 15:36:03 +0700

    New Glenda theme, and custom forward behavior

--- apps/web/src/theme.ts
+++ apps/web/src/theme.ts
@@ -91,6 +91,8 @@ export function enumerateThemes(): { [key: string]: string } {
         "light": _t("common|light"),
         "light-high-contrast": _t("theme|light_high_contrast"),
         "dark": _t("common|dark"),
+        // Custom theme
+        "glenda-light": "Glenda Light",
     };
     const customThemes = SettingsStore.getValue("custom_themes") || [];
     const customThemeNames: Record<string, string> = {};
